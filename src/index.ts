b7e4f2d117c7f3fa8bda7c27fe6f82ca39994985
Dinexx5 <Dinexx298@gmail.com>
2022-12-06 19:29:20 +0300

    igraem c git

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get('/', (req: Request, res: Response) => {
-    let helloSANYA = 'Hello SANYA ti krasava brat otvechau!';
+    let helloSANYA = 'Hello SANYA ti krasava brat otvechau voobche!';
     res.send(helloSANYA)
 })
 
